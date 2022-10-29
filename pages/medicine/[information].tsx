062ce0890ac23de8663a46a49877c1c04892bf75
김관우 <Kimkwanwoo@gimgwanhyeong-ui-MacBookPro.local>
2022-10-30 00:14:07 +0900

    증상으로 검색하는 페이지도 이전 검색 결과 가져올 수 있게 수정

--- pages/medicine/[information].tsx
+++ pages/medicine/[information].tsx
@@ -24,40 +24,57 @@ const Information: NextPage = () => {
   return (
     <section>
       {/* 이미지 */}
-      <Image
-        src={query.itemImage}
-        alt={query.itemName}
-        width={100}
-        height={100}
-      ></Image>
+      {query.itemImage && (
+        <Image
+          src={query.itemImage}
+          alt={query.itemName}
+          width={100}
+          height={100}
+        ></Image>
+      )}
+
       {/* 약 이름 */}
-      <h2>{query.itemName}</h2>
+      {query.itemName && <h2>{query.itemName}</h2>}
 
       {/* 복용 */}
-      <div>
-        <h4>복용 방법</h4>
-        <span>{query.useMethodQesitm.replace(regex, " ")}</span>
-      </div>
+      {query.useMethodQesitm && (
+        <div>
+          <h4>복용 방법</h4>
+          <span>{query.useMethodQesitm.replace(regex, " ")}</span>
+        </div>
+      )}
 
       {/* 효과 */}
-      <div>
-        <h4>효과</h4>
-        <p>{query.efcyQesitm.replace(regex, " ")}</p>
-      </div>
+      {query.efcyQesitm && (
+        <div>
+          <h4>효과</h4>
+          <p>{query.efcyQesitm.replace(regex, " ")}</p>
+        </div>
+      )}
 
       {/* 주의사항 */}
       <div>
-        <h4>복용 시 주의해야할 약</h4>
-        <p>{query.intrcQesitm.replace(regex, " ")}</p>
-        <h4>복용 시 주의해야할 이상반응</h4>
-        <p>{query.seQesitm.replace(regex, " ")}</p>
+        {query.intrcQesitm && (
+          <>
+            <h4>복용 시 주의해야할 약</h4>
+            <p>{query.intrcQesitm.replace(regex, " ")}</p>
+          </>
+        )}
+        {query.seQesitm && (
+          <>
+            <h4>복용 시 주의해야할 이상반응</h4>
+            <p>{query.seQesitm.replace(regex, " ")}</p>
+          </>
+        )}
       </div>
 
       {/* 보관방법 */}
-      <div>
-        <h4>보관 방법</h4>
-        <p>{query.depositMethodQesitm.replace(regex, " ")}</p>
-      </div>
+      {query.depositMethodQesitm && (
+        <div>
+          <h4>보관 방법</h4>
+          <p>{query.depositMethodQesitm.replace(regex, " ")}</p>
+        </div>
+      )}
 
       <button type="button" onClick={() => router.back()}>
         뒤로가기
