0d029d11c6be9a44bb7f0c2d150b568ec84a1ce1
kykson2 <40460377+kykson2@users.noreply.github.com>
2022-11-09 00:28:36 +0900

    Main 페이지 페이지 이동 시 링크에서 useRouter로 변경, 버튼에 style 적용

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,21 +1,20 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import StyledMainButton from "../../styles/StyledMainButton";
 
-const index: NextPage = () => {
+const Main: NextPage = () => {
+  const router = useRouter();
   return (
     <div>
-      <div>
-        <Link href={{ pathname: "../search" }}>
-          <a>검색해서 찾을래요</a>
-        </Link>
-      </div>
-      <div>
-        <Link href={{ pathname: "../symptoms" }}>
-          <a>증상으로 찾을래요</a>
-        </Link>
-      </div>
+      <StyledMainButton onClick={() => router.push("/search")}>
+        <a>검색해서 찾을래요</a>
+      </StyledMainButton>
+      <StyledMainButton onClick={() => router.push("/symptoms")}>
+        <a>증상으로 찾을래요</a>
+      </StyledMainButton>
     </div>
   );
 };
 
-export default index;
+export default Main;
---
+++ styles/StyledMainButton.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const StyledMainButton = styled.button`
+  width: 300px;
+  height: 300px;
+
+  border: 1px solid;
+  border-radius: 30px;
+
+  background-color: #4287f5;
+
+  font-size: 30px;
+  font-weight: bold;
+  color: #fff;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #376cbf;
+  }
+`;
+
+export default StyledMainButton;
